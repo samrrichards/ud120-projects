2ff50457c44525fd226c4ef3adeb698a62c0ac15
Sam Richards <samrrichards@gmail.com>
2017-01-22 17:56:40 -0800

    completed choose your own algorithm project

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from time import time
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -17,28 +18,40 @@ bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 
 
 #### initial visualization
-plt.xlim(0.0, 1.0)
-plt.ylim(0.0, 1.0)
-plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
-plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
-plt.legend()
-plt.xlabel("bumpiness")
-plt.ylabel("grade")
-plt.show()
+# plt.xlim(0.0, 1.0)
+# plt.ylim(0.0, 1.0)
+# plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+# plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+# plt.legend()
+# plt.xlabel("bumpiness")
+# plt.ylabel("grade")
+# plt.show()
 ################################################################################
 
+# I built an AdaBoost classifier using SVC estimators.
+# I tested it with some other algorithms, but this gave me the best results.
 
-### your code here!  name your classifier object clf if you want the 
-### visualization code (prettyPicture) to show you the decision boundary
+# The accuracy is around 92.8%... not quite the 93.6% Sebastian and Katie report,
+# but still pretty close!
 
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.svm import SVC
 
+classify = AdaBoostClassifier(base_estimator=SVC(C=100000), n_estimators=100, algorithm='SAMME')
 
+print "Starting to train algorithm!"
 
+t0 = time()
+classify.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
+t1 = time()
+classify.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
 
+print classify.score(features_test, labels_test)
 
-
-try:
-    prettyPicture(clf, features_test, labels_test)
-except NameError:
-    pass
+# try:
+#     prettyPicture(classify, features_test, labels_test)
+# except NameError:
+#     pass
